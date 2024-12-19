80287f6712c0412d1367fdd2fc51507489f27dba
AgileBite <agilebite.3@gmail.com>
2024-12-19 10:54:38 +0530

    final changes

--- chat_system/chat/serializers/message_serializers.py
+++ chat_system/chat/serializers/message_serializers.py
@@ -3,9 +3,13 @@ from chat.models import Message,Thread
 from chat.serializers.participant_serializers import ThreadParticipantBasicSerializer 
 
 class MessageSerializer(serializers.ModelSerializer):
-    participant = ThreadParticipantBasicSerializer(read_only = True)
     thread      = serializers.PrimaryKeyRelatedField(queryset=Thread.objects.all(), required=False)
     class Meta:
         model  = Message
         fields = '__all__'
+
+
+
+
+
 
